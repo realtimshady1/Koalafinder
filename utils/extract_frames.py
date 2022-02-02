1d9dc4e4e0bdee695f623cb2ae6030f65570fd7f
Timothy Guo <Timothy.guo3@gmail.com>
2022-02-02 19:56:49 +1100

    dev iteration

--- utils/extract_frames.py
+++ utils/extract_frames.py
@@ -1,22 +1,38 @@
 import cv2
 import os
 import sys
+from tqdm import tqdm
+from time import time
+from threading import Thread
+from queue import Queue
+
+def read_frames(frame_list, video_file, frames_queue):
+    cap = cv2.VideoCapture(video_file)
+    count = 0
+    for frame in frame_list:
+        if frame != count:
+            cap.set(1, frame)
+            count = frame
+        img = cap.read()[1]
+        count += 1
+        frames_queue.put(img)
+
+def write_frames(frame_list, folder, frames_queue):
+    for frame in tqdm(frame_list):
+        img = frames_queue.get(timeout=1)
+        cv2.imwrite(os.path.join(folder,str(frame).zfill(8))+'.jpg', img)
+
 
 def extract_frames(video_file, folder):
-    cap = cv2.VideoCapture(video_file)
+    start_time = time()
+    frames_queue = Queue(maxsize=1)
 
-    frame_list = [int(os.path.splitext(frame)[0]) for frame in os.listdir(folder)]
-    count = 0
-    while True:
-        if count in frame_list:
-            #cap.set(2,int(frame))
-            is_read, img = cap.read()
-            if not is_read:
-                break
-            cv2.imwrite(os.path.join(folder,str(count).zfill(8))+'.jpg', img)
-            count += 1
-        else:
-            count += 1
+    frame_list = sorted([int(os.path.splitext(frame)[0]) for frame in os.listdir(folder)])[:100]
+
+    Thread(target=read_frames, args=(frame_list, video_file, frames_queue)).start()
+    Thread(target=write_frames, args=(frame_list, folder, frames_queue)).start()
+
+    #print('Elapsed time: ', time()-start_time)
 
 if __name__=='__main__':
     if len(sys.argv) > 2:
