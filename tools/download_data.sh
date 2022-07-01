3fb559c22f4c666d76ebf8d101f6d30ead783273
jwood13 <jwood13@users.noreply.github.com>
2022-07-01 15:25:29 +1000

    Correct download_data.sh
    Previously bugged out with incomplete lines

--- tools/download_data.sh
+++ tools/download_data.sh
@@ -2,8 +2,8 @@
 NAME="tmp.rar"
 echo "Start download..."
 while read ID FILE; do
-    CMD=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google$
-    wget -nv --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=${CMD}&id=${ID}" -O ${NAME} $
+    CMD=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=${ID}" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+    wget -nv --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=${CMD}&id=${ID}" -O ${NAME} && rm -rf /tmp/cookies.txt
     echo "Downloading $FILE..."
     mkdir $FILE
     unrar e $NAME -idq -y $FILE
